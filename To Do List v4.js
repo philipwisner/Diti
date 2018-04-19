cabc2403747e357b3b2246ee6bc9003a8f7a6a2a
philipwisner <philipwisner@gmail.com>
2018-04-18 21:41:52 -0400

    finish displaytodo

--- To Do List v4.js
+++ To Do List v4.js
@@ -5,8 +5,17 @@
 var todoList = {
     todos: [],
     displayTodos: function () {
-        for (var i=0; i < todos.length; i++) {
-            console.log(todos[i].todoText);
+        console.log('My ToDos:')
+        if (this.todos.length === 0) {
+            console.log('You have no todos')
+        } else {            
+            for (var i=0; i < this.todos.length; i++) {
+                if (this.todos[i].completed) {
+                    console.log('(x)' + this.todos[i].todoText);
+                } else {
+                    console.log('( )' + this.todos[i].todoText);
+                }
+          }
         }
     },
     addTodo: function (todoText) {
