f6ee3997408c6a2f5086671ea33b51ae4c914a17
philipwisner <philipwisner@gmail.com>
2018-04-18 20:53:01 -0400

    to-do v2 & v3

---
+++ To Do List.js
@@ -0,0 +1,76 @@
+//TO DO LIST V1
+
+var todos = ['item 1', 'item 2', 'item 3'];
+
+//DISPLAY TO DO LIST
+function displayTodos() {
+    window.console.log('My Todos:', todos);
+}
+
+
+
+//ADD TO DO TO LIST
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodos();
+}
+
+//EDIT TO DO IN LIST
+function editTodo(position, newValue) {
+    todos[position] = newValue;
+    displayTodos();
+}
+
+
+//DELETE TODO
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+function runAll() {
+    displayTodos();
+    addTodo();
+    editTodo();
+    deleteTodo();
+}
+
+runAll();
+
+
+
+TO DO LIST V2
+var todoList2 = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function () {
+        window.console.log('My ToDos:', this.todos);
+    },
+    addTodo: function (todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    editTodo: function (position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function (position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
+
+var todoList = {
+    editTodo: function (position, newText) {
+        this.todos[position].todoText = newText;
+        this.displayTodos();
+    },
+    deleteTodo: function (position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
