6b42ca58a242e3dc997030dbc4260d1930d98432
philipwisner <philipwisner@gmail.com>
2018-04-20 02:24:29 -0400

    delete button

--- To Do List v5.js
+++ To Do List v5.js
@@ -1,46 +1,26 @@
-//TO DO LIST 54
-
-//Using an object, but adding a completed section to todos array
+//TO DO LIST V%
 
 var todoList = {
     todos: [],
-    displayTodos: function () {
-        console.log('My ToDos:')
-        if (this.todos.length === 0) {
-            console.log('You have no todos')
-        } else {            
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed) {
-                    console.log('(x)', this.todos[i].todoText);
-                } else {
-                    console.log('( )', this.todos[i].todoText);
-                }
-          }
-        }
-    },
     addTodo: function (todoText) {
         this.todos.push({
             todoText: todoText,
-            completed: false,
+            completed: false
         });
-        this.displayTodos();
     },
     editTodo: function (position, newText) {
         this.todos[position].todoText = newText;
-        this.displayTodos();
     },
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
-        this.displayTodos();
     },
     toggleCompleted: function (position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
     toggleAll: function () {
         var totalTodos = this.todos.length;
-        var completedTodos = 0;;
+        var completedTodos = 0;
         
         for (var i = 0; i < totalTodos; i++) {
             if (this.todos[i].completed) {
@@ -57,10 +37,7 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-        this.displayTodos();
     }
-// If evertyhing is true (completed), make everything false
-// Otherwise making everything true
 };
 
 
@@ -86,8 +63,7 @@ var handlers = {
 		view.displayTodos();
     },
     toggleCompleted: function() {
-        var completedTodoPosition = document.getElementById('toggleTodoPositionInput');
- 		todoList.toggleCompleted(completedTodoPosition.valueAsNumber);
+        var completedTodoPosition = document.getElementById('toggleTodoPositionInput');		todoList.toggleCompleted(completedTodoPosition.valueAsNumber);
         completedTodoPosition.value = '';
 		view.displayTodos();
     },
@@ -113,5 +89,10 @@ var view = {
 			todoItem.textContent = todoTextWithCompletion;
 			todoUL.appendChild(todoItem);
         }
-    }
+    },
+	createDeleteButton: function() {
+		var deleteButton = document.createElement('button');
+		deleteButton.textContent = 'Delete';
+		deleteButton.className = 'deleteButton';
+	}
 };
